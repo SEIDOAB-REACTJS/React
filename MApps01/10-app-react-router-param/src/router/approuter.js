455e4ef40a679c30e27d29da8b435a18dbe929fb
Martin <martin_lenart@icloud.com>
2024-06-04 10:26:48 +0200

    update

---
+++ MApps01/10-app-react-router-param/src/router/approuter.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Routes, Route } from "react-router-dom";
+
+import Home from '../pages/home';
+import About from '../pages/about';
+import Page1 from '../pages/page1';
+import {Musicalbums} from '../pages/musicalbums';
+import {Animals} from '../pages/animals';
+
+
+export function AppRouter() {
+  return (
+    <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/page1' element={<Page1/>}></Route>
+        <Route path='/music' element={<Musicalbums/>}></Route>
+        <Route path='/animals/:name' element={<Animals/>}></Route>
+    </Routes>
+  )
+}
