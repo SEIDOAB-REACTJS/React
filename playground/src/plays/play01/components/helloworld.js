16091c5a9c1c9b2a6185da59c15e11cf30d58c5e
Martin <martin_lenart@icloud.com>
2025-04-02 16:24:35 +0200

    main correction

---
+++ playground/src/plays/play01/components/helloworld.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { uniqueId, seedGenerator } from '../../../services/seido-helpers';
+
+export function Helloworld() {
+  return (
+    <>
+    <div>helloworld</div>
+    <div>hello again</div>
+    </>
+  )
+}
