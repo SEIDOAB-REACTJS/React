403e90bf8e5b448364c3b1264881c7b163baaef0
Martin <martin_lenart@icloud.com>
2025-04-02 08:31:30 +0200

    update

---
+++ playground/src/apps/App05.js
@@ -0,0 +1,30 @@
+import { seedGenerator } from '../services/seido-helpers';
+import Person from '../models/person';
+import Animal from '../models/animal';
+
+
+//https://legacy.reactjs.org/docs/strict-mode.html#detecting-unexpected-side-effects
+//You will notice React in Dev mode mounts the components twice.
+//This is by intention to allow you to ease debugging. E.g. you can catch unmounting problems
+
+export function App05_01() {
+
+  return (
+    <>
+    </>
+  );}
+
+export function App05_02() {
+
+  return (
+    <>
+    </>
+  );}
+
+export function App05_03() {
+
+  return (
+    <>
+    </>
+  );}
+
